46487b5a79806ded7c56b3b1a350c68a80d92d31
m1_novotny <m1_novotny@utb.cz>
2022-12-10 16:08:44 +0100

    DatabaseEntities.kt - created a Room entity

--- app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
@@ -16,8 +16,18 @@
 
 package com.example.android.devbyteviewer.database
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 
 /**
- * Database entities go in this file. These are responsible for reading and writing from the
- * database.
- */
+* DatabaseVideo represents a video entity in the database.
+*/
+@Entity
+data class DatabaseVideo constructor(
+    @PrimaryKey
+    val url: String,
+    val updated: String,
+    val title: String,
+    val description: String,
+    val thumbnail: String)

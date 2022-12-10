d3315cd209b5baa7b98f63194105b2f75ec794a4
m1_novotny <m1_novotny@utb.cz>
2022-12-10 16:11:34 +0100

    DatabaseEntities.kt - created an asDomainModel function to convert DatabaseVideo database objects into domain objects

--- app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
@@ -18,6 +18,7 @@ package com.example.android.devbyteviewer.database
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.android.devbyteviewer.domain.DevByteVideo
 
 
 /**
@@ -31,3 +32,16 @@ data class DatabaseVideo constructor(
     val title: String,
     val description: String,
     val thumbnail: String)
+/**
+ * Map DatabaseVideos to domain entities
+ */
+fun List<DatabaseVideo>.asDomainModel(): List<DevByteVideo> {
+    return map {
+        DevByteVideo(
+            url = it.url,
+            title = it.title,
+            description = it.description,
+            updated = it.updated,
+            thumbnail = it.thumbnail)
+    }
+}

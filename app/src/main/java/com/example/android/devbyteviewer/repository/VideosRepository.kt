f90d72cdd15061dd397d0ff7e767cbd180a3698f
m1_novotny <m1_novotny@utb.cz>
2022-12-10 16:35:57 +0100

    VideosRepository.kt - added a LiveData object to retrieve data from the database

--- app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -16,7 +16,11 @@
 
 package com.example.android.devbyteviewer.repository
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
 import com.example.android.devbyteviewer.database.VideosDatabase
+import com.example.android.devbyteviewer.database.asDomainModel
+import com.example.android.devbyteviewer.domain.DevByteVideo
 import com.example.android.devbyteviewer.network.DevByteNetwork
 import com.example.android.devbyteviewer.network.asDatabaseModel
 import kotlinx.coroutines.Dispatchers
@@ -32,6 +36,11 @@ class VideosRepository(private val database: VideosDatabase) {
             database.videoDao.insertAll(playlist.asDatabaseModel())
         }
     }
+    val videos: LiveData<List<DevByteVideo>> = Transformations.map(database.videoDao.getVideos()) {
+        it.asDomainModel()
+    }
+
+
 
 
 
